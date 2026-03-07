38d640eeb27af00a068be71c2e24b4751f5b0924
Ivon <ivongarcia@siticl.com>
2026-03-07 17:48:29 -0600

    refactor: update IAsteroid

--- src/nasa/interface/asteroid.ts
+++ src/nasa/interface/asteroid.ts
@@ -1,8 +1,18 @@
 export interface IAsteroid {
   name: string;
+  is_potentially_hazardous_asteroid: boolean;
   estimated_diameter: {
     kilometers: {
+      estimated_diameter_min: number;
       estimated_diameter_max: number;
     };
   };
+  close_approach_data: {
+    relative_velocity: {
+      kilometers_per_hour: string;
+    };
+    miss_distance: {
+      kilometers: string;
+    };
+  }[];
 }
